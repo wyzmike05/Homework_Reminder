dc45c479adab30d5a73e19fbb052442133fd81ad
Yunze Wu <wyzmike@hotmail.com>
2024-10-21 16:49:50 +0800

    Renew some method

--- Homework_Reminder.py
+++ Homework_Reminder.py
@@ -24,8 +24,8 @@ class W_Reminder(QWidget):
         super().__init__()
 
         # set window title window size and window position
-        self.setWindowTitle("item Reminder")
-        self.resize(400, 600)
+        self.setWindowTitle("task Reminder")
+        self.resize(1000, 1000)
         self.set_window_position()
 
         # create layout
@@ -64,41 +64,50 @@ class W_Reminder(QWidget):
         # save button
         self.save_button = QPushButton("Save")
         layout.addWidget(self.save_button)
+        # connect save button with add_task
+        self.save_button.clicked.connect(self.add_task)
 
-        # display item to do
-        self.item_to_do_label = QLabel("item To Do:")
-        layout.addWidget(self.item_to_do_label)
+        # display task to do
+        self.task_todo_label = QLabel("task To Do:")
+        layout.addWidget(self.task_todo_label)
 
-        self.item_to_do = QListWidget()
-        layout.addWidget(self.item_to_do)
+        self.task_todo = QListWidget()
+        layout.addWidget(self.task_todo)
+
+        # display task done
+        self.task_done_label = QLabel("task done:")
+        layout.addWidget(self.task_done_label)
+
+        self.task_done = QListWidget()
+        layout.addWidget(self.task_done)
         # TODO
-
-        # connect save button with add_item
-        self.save_button.clicked.connect(self.add_item)
 
         # set layout
         self.setLayout(layout)
 
-        # load item
-        self.load_item()
+        # load task
+        self.load_task()
 
     def set_window_position(self):
         """
         set window position
         """
         qr = self.frameGeometry()
+        # get central point
         central_point = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(central_point)
         self.move(qr.topLeft())
 
-    def add_item(self):
+    def add_task(self):
         """
         write the file with input
         """
+
         subject = self.subject_input.toPlainText().strip()
         assignment_date = self.assignment_date_input.dateTime()
         content = self.assignment_content_input.toPlainText().strip()
         due_time = self.due_time_input.dateTime()
+        done = False
 
         if not subject or not content:
             QMessageBox.warning(self, "Error", "Subject and content cannot be empty!")
@@ -112,15 +121,16 @@ class W_Reminder(QWidget):
         assignment_date = assignment_date.toString("yyyy-MM-dd HH:mm")
         due_time = due_time.toString("yyyy-MM-dd HH:mm")
 
-        item_data = {
+        task_data = {
             "subject": subject,
             "assignment_date": assignment_date,
             "content": content,
             "due_time": due_time,
+            "done": done,
         }
 
-        if os.path.exists("item_data.json"):
-            with open("item_data.json", "r") as file:
+        if os.path.exists("task_data.json"):
+            with open("task_data.json", "r") as file:
                 try:
                     data = json.load(file)
                 except json.JSONDecodeError:
@@ -128,60 +138,151 @@ class W_Reminder(QWidget):
         else:
             data = []
 
-        data.append(item_data)
+        data.append(task_data)
 
         data = sorted(data, key=lambda x: x["due_time"])
 
-        with open("item_data.json", "w") as file:
+        with open("task_data.json", "w") as file:
             json.dump(data, file, indent=4)
 
         self.subject_input.clear()
         self.assignment_content_input.clear()
 
-        self.load_item()
+        self.load_task()
 
         # TODO
 
-    def load_item(self):
-        # 清空当前显示的列表
-        self.item_to_do.clear()
+    def load_task(self):
+        """
+        with state todo (False) and done (True)
+        """
+        # clear the lists
+        self.task_todo.clear()
+        self.task_done.clear()
 
-        # 检查文件是否存在
-        if os.path.exists("item_data.json"):
-            with open("item_data.json", "r") as file:
+        if os.path.exists("task_data.json"):
+            with open("task_data.json", "r") as file:
                 try:
                     data = json.load(file)
                 except json.JSONDecodeError:
                     data = []
 
-            # 遍历数据并将每条任务显示在列表中
-            for item_data in data:
-                subject = item_data["subject"]
-                assignment_date = item_data["assignment_date"]
-                due_time = item_data["due_time"]
-                content = item_data["content"]
+            for task_data in data:
+                # read file
+                subject = task_data["subject"]
+                assignment_date = task_data["assignment_date"]
+                due_time = task_data["due_time"]
+                content = task_data["content"]
+                done = task_data["done"]
 
-                # 将任务内容组合成一个可视化的字符串
-                item_text = f"Subject: {subject}\nContent: {content}\nAssigned:{assignment_date}\nDue: {due_time}"
+                task_widget = QWidget()
+                task_layout = QHBoxLayout()
+                task_widget.setLayout(task_layout)
+                list_task = QListWidgetItem()
 
-                # 创建QListWidgetItem并添加到QListWidget
-                list_item = QListWidgetItem(item_text)
-                self.item_to_do.addItem(list_item)
+                # task label
+                task_label = QLabel(
+                    f"Subject: {subject}\nContent: {content}\nAssigned: {assignment_date}\nDue: {due_time}"
+                )
+                task_layout.addWidget(task_label)
+
+                # edit button
+                edit_button = QPushButton("Edit")
+                edit_button.clicked.connect(lambda: self.edit_task(task_data))
+                task_layout.addWidget(edit_button)
+
+                # delete button
+                delete_button = QPushButton("Delete")
+                delete_button.clicked.connect(lambda: self.delete_task(task_data))
+                task_layout.addWidget(delete_button)
+
+                if done:
+                    uncomplete_button = QPushButton("Uncomplete")
+                    uncomplete_button.clicked.connect(
+                        lambda: self.uncomplete_task(task_data)
+                    )
+                    task_layout.addWidget(uncomplete_button)
+
+                    self.task_done.addItem(list_task)
+
+                    self.task_done.setItemWidget(list_task, task_widget)
+                else:
+                    complete_button = QPushButton("Complete")
+                    complete_button.clicked.connect(
+                        lambda: self.complete_task(task_data)
+                    )
+                    task_layout.addWidget(complete_button)
+
+                    self.task_todo.addItem(list_task)
+
+                    self.task_todo.setItemWidget(list_task, task_widget)
+
+                list_task.setSizeHint(task_widget.sizeHint())
         # TODO
 
-    def finish_item(self):
+    def complete_task(self, task_data):
+        if os.path.exists("task_data.json"):
+            with open("task_data.json", "r") as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
+            try:
+                index = data.index(task_data)
+            except ValueError:
+                print("Task Not Found...")
+                return
+            data[index]["done"] = True
+
+            with open("task_data.json", "w") as file:
+                json.dump(data, file, indent=4)
+
+        # TODO
+
+        self.load_task()
+
+    def uncomplete_task(self, task_data):
+        if os.path.exists("task_data.json"):
+            with open("task_data.json", "r") as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
+            try:
+                index = data.index(task_data)
+            except ValueError:
+                print("Task Not Found...")
+                return
+            data[index]["done"] = False
+
+            with open("task_data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        # TODO
+        self.load_task()
+
+    def delete_task(self, task_data):
+        if os.path.exists("task_data.json"):
+            with open("task_data.json", "r") as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
+            try:
+                data.remove(task_data)
+            except ValueError:
+                return
+
+            with open("task_data.json", "w") as file:
+                json.dump(data, file, indent=4)
+
+        # TODO
+        self.load_task()
+
+    def remind_task(self):
         pass
         # TODO
 
-    def delete_item(self):
-        pass
-        # TODO
-
-    def remind_item(self):
-        pass
-        # TODO
-
-    def edit_item(self):
+    def edit_task(self):
         pass
         # TODO
 
